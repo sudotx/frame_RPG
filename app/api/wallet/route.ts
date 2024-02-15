ccd57f02901c7f84fffb2ce0e942896ae65a69bf
sudotx <64108848+sudotx@users.noreply.github.com>
2024-02-15 02:02:16 +0100

    chore: clean

--- app/api/wallet/route.ts
+++ app/api/wallet/route.ts
@@ -26,17 +26,19 @@ export async function POST(req: NextRequest): Promise<Response> {
   const { fid, isValid } = await parseFrameRequest(frameRequest);
   if (!fid || !isValid) return new NextResponse(errorFrame);
 
-  // Query Farcaster Registry contract to get owner address from fid
-  const ownerAddress = await getOwnerAddressFromFid(fid);
-  if (!ownerAddress) return new NextResponse(errorFrame);
+  // // Query Farcaster Registry contract to get owner address from fid
+  // const ownerAddress = await getOwnerAddressFromFid(fid);
+  // if (!ownerAddress) return new NextResponse(errorFrame);
 
-  // Generate an embedded wallet associated with the fid
-  const embeddedWalletAddress = await createOrFindSmartWalletForFid(
-    fid,
-    ownerAddress
+  // // Generate an embedded wallet associated with the fid
+  // const embeddedWalletAddress = await createOrFindSmartWalletForFid(
+  //   fid,
+  //   ownerAddress
+  // );
+  // if (!embeddedWalletAddress) return new NextResponse(errorFrame);
+  return new NextResponse(
+    mintFrame("0x0000000000000000000000000000000000000000")
   );
-  if (!embeddedWalletAddress) return new NextResponse(errorFrame);
-  return new NextResponse(mintFrame(embeddedWalletAddress));
 
   // return new NextResponse(createWalletFrame(embeddedWalletAddress));
 }
