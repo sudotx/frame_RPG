c8173176a6aba04591f507b9175eb900d2ed9bee
sudotx <64108848+sudotx@users.noreply.github.com>
2024-02-16 21:26:33 +0100

    chore: peng up weapon mint screen

--- app/api/mint/[address]/route.ts
+++ app/api/mint/[address]/route.ts
@@ -8,11 +8,12 @@ import { FrameRequest } from "@coinbase/onchainkit";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<Response> {
-  // let frameRequest: FrameRequest | undefined;
-  // // Parse and validate request from Frame for fids
-  // frameRequest = await req.json();
+  let frameRequest: FrameRequest | undefined;
+  // Parse and validate request from Frame for fids
+  frameRequest = await req.json();
   // if (!frameRequest)
   //   throw new Error("Could not deserialize request from frame");
+  const buttonId = frameRequest?.untrustedData.buttonIndex;
 
   // const { fid, isValid } = await parseFrameRequest(frameRequest);
   // if (!fid || !isValid) return new NextResponse(errorFrame);
@@ -21,10 +22,17 @@ export async function POST(req: NextRequest): Promise<Response> {
   if (typeof address !== "string") return new NextResponse(errorFrame);
 
   // Send NFT to the user's wallet
-  const tx = createTokenMintIntent(address);
-  if (!tx) {
-    return new NextResponse(errorFrame);
+  // const tx = createTokenMintIntent(address);
+  // if (!tx) {
+  //   return new NextResponse(errorFrame);
+  // } else {
+  //   return new NextResponse(knightsHorseBackFrame);
+  // }
+  if (buttonId === 1) {
+    // user selects sword
+    return new NextResponse(knightsHorseBackFrame);
   } else {
+    // user selects spear
     return new NextResponse(knightsHorseBackFrame);
   }
 }
