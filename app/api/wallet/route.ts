b43f3d0ee9f44544d052fe13cf64327280c6cb66
sudotx <64108848+sudotx@users.noreply.github.com>
2024-02-16 06:18:45 +0100

    debug 7

--- app/api/wallet/route.ts
+++ app/api/wallet/route.ts
@@ -13,14 +13,14 @@ export async function POST(req: NextRequest): Promise<Response> {
   let frameRequest: FrameRequest | undefined;
 
   // Parse and validate request from Frame for fid
-  frameRequest = await req.json();
+  // frameRequest = await req.json();
 
-  if (!frameRequest) {
-    throw new Error("Could not deserialize request from frame");
-  }
+  // if (!frameRequest) {
+  //   throw new Error("Could not deserialize request from frame");
+  // }
 
-  const { fid, isValid } = await parseFrameRequest(frameRequest);
-  if (!fid || !isValid) return new NextResponse(errorFrame);
+  // const { fid, isValid } = await parseFrameRequest(frameRequest);
+  // if (!fid || !isValid) return new NextResponse(errorFrame);
 
   // // Query Farcaster Registry contract to get owner address from fid
   // const ownerAddress = await getOwnerAddressFromFid(fid);
