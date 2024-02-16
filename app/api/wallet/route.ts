d9e71dccce626ceff0476895a7cbb136e2380b9f
sudotx <64108848+sudotx@users.noreply.github.com>
2024-02-16 05:32:25 +0100

    chore: debug

--- app/api/wallet/route.ts
+++ app/api/wallet/route.ts
@@ -20,9 +20,9 @@ export async function POST(req: NextRequest): Promise<Response> {
   const { fid, isValid } = await parseFrameRequest(frameRequest);
   if (!fid || !isValid) return new NextResponse(errorFrame);
 
-  // Query Farcaster Registry contract to get owner address from fid
-  const ownerAddress = await getOwnerAddressFromFid(fid);
-  if (!ownerAddress) return new NextResponse(errorFrame);
+  // // Query Farcaster Registry contract to get owner address from fid
+  // const ownerAddress = await getOwnerAddressFromFid(fid);
+  // if (!ownerAddress) return new NextResponse(errorFrame);
 
   // // Generate an embedded wallet associated with the fid
   // const embeddedWalletAddress = await createOrFindSmartWalletForFid(
