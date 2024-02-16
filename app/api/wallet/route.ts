cd9f576c328cf89389df359b37654f2ed47649d6
sudotx <64108848+sudotx@users.noreply.github.com>
2024-02-16 05:03:20 +0100

    chore: debug

--- app/api/wallet/route.ts
+++ app/api/wallet/route.ts
@@ -1,4 +1,10 @@
-import { errorFrame, mintFrame, parseFrameRequest } from "@/services/farcaster";
+import { createOrFindSmartWalletForFid } from "@/services/embedded-wallet";
+import {
+  errorFrame,
+  getOwnerAddressFromFid,
+  mintFrame,
+  parseFrameRequest,
+} from "@/services/farcaster";
 import { FrameRequest } from "@coinbase/onchainkit";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -11,30 +17,21 @@ export async function POST(req: NextRequest): Promise<Response> {
   if (!frameRequest)
     throw new Error("Could not deserialize request from frame");
 
-  console.log("frame-request", frameRequest);
-
   const { fid, isValid } = await parseFrameRequest(frameRequest);
   if (!fid || !isValid) return new NextResponse(errorFrame);
 
-  console.log("fid", fid);
-  console.log("isValid", isValid);
+  // Query Farcaster Registry contract to get owner address from fid
+  const ownerAddress = await getOwnerAddressFromFid(fid);
+  if (!ownerAddress) return new NextResponse(errorFrame);
 
-  // // Query Farcaster Registry contract to get owner address from fid
-  // const ownerAddress = await getOwnerAddressFromFid(fid);
-  // if (!ownerAddress) return new NextResponse(errorFrame);
-
-  // // Generate an embedded wallet associated with the fid
-  // const embeddedWalletAddress = await createOrFindSmartWalletForFid(
-  //   fid,
-  //   ownerAddress
-  // );
-  // if (!embeddedWalletAddress) return new NextResponse(errorFrame);
-
-  return new NextResponse(
-    mintFrame(
-      "0x59d6d3118ccb1e9c54f9c701ca1e7140087236f86ab9ccdd9f0731a9ea84708"
-    )
+  // Generate an embedded wallet associated with the fid
+  const embeddedWalletAddress = await createOrFindSmartWalletForFid(
+    fid,
+    ownerAddress
   );
+  if (!embeddedWalletAddress) return new NextResponse(errorFrame);
+
+  return new NextResponse(mintFrame(embeddedWalletAddress));
 }
 
 export const dynamic = "force-dynamic";
